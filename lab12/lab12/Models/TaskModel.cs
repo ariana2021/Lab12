47593c4b861fa3c14000321877642d98bf40b05c
ariana2021 <ariana.azabache@tecsup.edu.pe>
2023-12-02 23:53:51 -0500

    Agregar archivos de proyecto.

---
+++ lab12/lab12/Models/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab12.Models
+{
+    public class TaskModel
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public string IsCompleted { get; set; }
+
+        public TaskModel() { }
+
+    }
+}
